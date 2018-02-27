b91d5450f85eb6b494947006b15e4852ac72d658
Stephen Stanton <stephenstanton10@gmail.com>
2018-02-26 20:19:00 -0500

    Changed project names

---
+++ Core/Enums/TimeOfDay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Enums
+{
+    public enum TimeOfDay
+    {
+        SmallHours = 0, // 12am-6am
+        Morning = 1, // 6am-12pm
+        Afternoon = 2, // 12pm-6pm
+        Night = 3 // 6pm-12am
+    }
+}
